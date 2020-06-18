ad32d6620fdc1b4aac802950677b1314b4edafab
ghafran <ghafranabbas@gmail.com>
2020-06-17 23:05:27 -0400

    add events subscriptions

---
+++ test.js
@@ -0,0 +1,33 @@
+
+var vitalchat = require('./index');
+var client = new vitalchat({
+    host: process.env.VITALCHAT_HOST,
+    key: process.env.VITALCHAT_KEY,
+    secret: process.env.VITALCHAT_SECRET
+});
+
+client.on('event', (event) => {
+    console.log(event);
+});
+
+client.on('log', (log) => {
+    console.log(log);
+});
+
+client.on('error', (err) => {
+    console.log(err);
+});
+
+(async () => {
+    client.listen();
+    var devices = await client.devices();
+    console.log(devices);
+    var data = await client.call({
+        device_id: devices[0].device_id,
+        caller_id: 'test caller',
+        action: 'knock'
+    });
+    console.log(data);
+})().catch((err) => {
+    console.error(err);
+});

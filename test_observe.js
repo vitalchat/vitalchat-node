5affb01b05204b3f9939aa21612f93d7056035e8
Ghafran Abbas <ghafranabbas@gmail.com>
2023-10-26 12:46:49 -0400

    a

---
+++ test_observe.js
@@ -0,0 +1,35 @@
+var vitalchat = require('./index');
+var client = new vitalchat({
+	host: process.env.VITALCHAT_HOST,
+	key: process.env.VITALCHAT_KEY,
+	secret: process.env.VITALCHAT_SECRET,
+});
+
+client.on('event', (event) => {
+	console.log(JSON.stringify(event, null, '\t'));
+});
+
+client.on('log', (log) => {
+	console.log(JSON.stringify(log, null, '\t'));
+});
+
+client.on('error', (err) => {
+	console.log(err);
+});
+
+(async () => {
+	console.log('connecting to', process.env.VITALCHAT_HOST);
+	var devices = await client.devices();
+	console.log(devices);
+	if (devices.length === 0) {
+		console.log('no devices found');
+		return;
+	}
+	var data = await client.observe({
+		device: devices[0].device_id,
+		device_description: 'John Smith',
+	});
+	console.log(JSON.stringify(data, null, '\t'));
+})().catch((err) => {
+	console.error(err);
+});
